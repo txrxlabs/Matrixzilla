0f3d1ed41c90df88559623ccba2ee54278b291eb
Stephen M. Cameron <smcameron@yahoo.com>
2011-06-26 15:17:14 -0500

    Added cube.c, testcube.c

---
+++ MatrixPlayer/cube.c
@@ -0,0 +1,171 @@
+#include <string.h> /* for memset */
+#include "MatrixDef.h"
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.1415927
+
+static int xdim;
+static int ydim;
+
+static float ez = 15.0;
+
+static float cubescale = 7.0;
+// static float cubescale = 10.0;
+
+#define ARRAYSIZE(x) (sizeof((x)) / sizeof((x)[0]))
+
+struct point {
+	float x, y, z;
+} cubept2[] = {
+	{ 1, 1, 1 },
+	{ -1, 1, 1 },
+	{ -1, -1, 1 },
+	{ 1, -1, 1 },
+	{ 1, 1, -1 },
+	{ -1, 1, -1 },
+	{ -1, -1, -1 },
+	{ 1, -1, -1 },
+};
+
+struct point cubept3[ARRAYSIZE(cubept2)];
+struct point cubept[ARRAYSIZE(cubept2)];
+
+static float cubetransx = 15.0;
+static float cubetransy = 9.0;
+static float cubetransz = 30.0;
+
+char cube[] = { 0, 1, 2, 3, 0, 4, 5, 6, 7, 4,
+		-1, 7, 3, -1, 2, 6, -1, 5, 1, -1};
+
+
+#define CUBE_ITERATIONS 1500
+
+static void yrotate(struct point *p1, struct point *p2, int npoints, float angle)
+{
+	int i;
+	float cosa, sina;
+
+	cosa = cos(angle);
+	sina = sin(angle);
+
+	for (i = 0; i < npoints; i++) {
+		p2[i].x = p1[i].x * cosa - p1[i].z * sina;
+		p2[i].z = p1[i].x * sina + p1[i].z * cosa;
+		p2[i].y = p1[i].y;
+	}
+}
+
+static void zrotate(struct point *p1, struct point *p2, int npoints, float angle)
+{
+	int i;
+	float cosa, sina;
+
+	cosa = cos(angle);
+	sina = sin(angle);
+
+	for (i = 0; i < npoints; i++) {
+		p2[i].x = p1[i].x * cosa - p1[i].y * sina;
+		p2[i].y = p1[i].x * sina + p1[i].y * cosa;
+		p2[i].z = p1[i].z;
+	}
+}
+
+static void clear_board(char *board)
+{
+	memset(board, 0, xdim * ydim);
+}
+
+void orthographic(float x, float y, float z, float *dx, float *dy)
+{
+	*dx = x + z * 0.5;
+	*dy = y + z * 0.5;
+}
+
+void perspective(float x, float y, float z, float *dx, float *dy)
+{
+	*dx = (xdim / 2.0) + (ez / z) * (x - (xdim/2.0));
+	*dy = (ydim / 2.0) + (ez / z) * (y - (ydim/2.0));
+}
+
+static void (*projection)(float x, float y, float z, float *dx, float *dy) =
+	perspective;
+
+void draw_cube(char *board) 
+{
+	int i, n;
+	float x, y, z, lx, ly, nx, ny;
+	char x1, y1, x2, y2;
+
+	n = cube[0];
+	x = cubept[n].x * cubescale + cubetransx;
+	y = cubept[n].y * cubescale + cubetransy;
+	z = cubept[n].z * cubescale + cubetransz;
+	projection(x, y, z, &lx, &ly);
+
+	// orthographic(x, y, z, &lx, &ly);
+
+	for (i = 1; i < ARRAYSIZE(cube); i++) {
+		n = cube[i];
+		if (n == -1)
+			continue;
+		x = cubept[n].x * cubescale + cubetransx;
+		y = cubept[n].y * cubescale + cubetransy;
+		z = cubept[n].z * cubescale + cubetransz;
+		projection(x, y, z, &nx, &ny);
+
+		x1 = (char) lx;
+		y1 = (char) ly;
+		x2 = (char) nx;
+		y2 = (char) ny;
+		lx = nx;
+		ly = ny;
+		if (x1 < 0 || x1 >= xdim || x2 < 0 || x2 >= xdim)
+			continue;
+		if (y1 < 0 || y1 >= ydim || y2 < 0 || y2 >= ydim)
+			continue;
+		bline(board, x1, y1, x2, y2);
+	}
+}
+
+void docube(char *board, int x, int y, int seed)
+{
+	int i;
+
+	xdim = x;
+	ydim = y;
+	randomSeed(seed);
+	float zvel = 1.0;
+	float xvel = 1.0;
+	int angle = 0;
+	int angle2 = 0;
+
+	memcpy(&cubept, &cubept2, sizeof(cubept2));
+
+	for (i = 0; i < CUBE_ITERATIONS; i++) {
+		clear_board(board);
+		draw_cube(board);
+		displayBoard(board);
+#if 0
+		cubetransz += zvel;
+		if (cubetransz > 55)
+			zvel = -0.9;
+		if (cubetransz < 10.0)
+			xvel = 0.9;
+		cubetransx += xvel;
+		if (cubetransx > 25)
+			xvel = -0.9;
+		if (cubetransx < -25)
+			xvel = 0.9;
+#endif
+		yrotate(cubept2, cubept3, ARRAYSIZE(cubept), PI * angle / 180.0);
+		zrotate(cubept3, cubept, ARRAYSIZE(cubept), PI * angle2 / 180.0);
+		angle2 += 1;
+		if (angle2 > 360)
+			angle2 = 0;
+		angle += 2;
+		if (angle > 360)
+			angle = 0;
+	}
+}
+
---
+++ MatrixPlayer/testcube.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// char myboard[25*15];
+
+#if 0
+#define XDIM 79 
+#define YDIM 23
+#define X_SIZE 79
+#define Y_SIZE 23
+#else
+#define XDIM 25 
+#define YDIM 15
+#define X_SIZE 25 
+#define Y_SIZE 15 
+#endif
+
+char myboard[XDIM*YDIM];
+char *board = (char *) myboard;
+
+void randomSeed(int x)
+{
+	return;
+}
+
+char getrandom(char x);
+
+/* arduino style random conflicts with libc style random... */
+#define random(x) getrandom((x))
+
+#include "bline.c"
+#include "cube.c"
+
+char getrandom(char x)
+{
+	int y;
+	y = (((rand() & 0x0ff) * (int) x) / 256);
+	// printf("x = %d, y = %d\n", x, y);
+	return (char) y;
+}
+
+void displayBoard(char *board)
+{
+	int x, y;
+
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+	for (y = 0; y < YDIM; y++) {
+		for (x = 0; x < XDIM; x++) {
+			printf("%c", board[y * XDIM + x] ? '*' : '.');
+		}
+		printf("\n");
+	}
+	usleep(100000);	
+}
+
+int main(int argc, char *argv[])
+{
+	srand(250);
+	docube(board, XDIM, YDIM, 250);
+}
